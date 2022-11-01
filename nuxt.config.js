b26d0edf2547f345594fb33852b1eb2c0798ab3d
btm2021 <trinhminhbao@gmail.com>
2022-11-01 20:15:21 +0700

    asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,10 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
-
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  }
   // Build Configuration: https://go.nuxtjs.dev/config-build
 
 }
